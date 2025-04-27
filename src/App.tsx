08a47db709e9955b8eee4ebfc17bd9f4ed5e9cfa
gianlucagalota <gianluca.galota@gmail.com>
2025-04-27 21:20:25 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import ChatLayout from './chat/layout/chat-layout'
+import ChatPage from './chat/pages/chat-page'
+
+function App() {
+
+  return (
+    <>
+      {/* <AuthLayout /> */}
+      <ChatLayout>
+        <ChatPage />
+      </ChatLayout>
+    </>
+  )
+}
+
+export default App
